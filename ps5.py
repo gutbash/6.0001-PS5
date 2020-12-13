51e386d81fc9fb128b172c6d7178afefd82aff59
bazeroni <44552816+bazeroni@users.noreply.github.com>
2020-12-13 14:00:10 -0500

    parsed info storage object

--- ps5.py
+++ ps5.py
@@ -54,7 +54,24 @@ def process(url):
 
 # Problem 1
 
-# TODO: NewsStory
+# NewsStory
+class NewsStory(object):
+    def __init__(self, guid, title, description, link, pubdate):
+        self.guid = guid
+        self.title = title
+        self.description = description
+        self.link = link
+        self.pubdate = pubdate
+    def get_guid(self):
+        return self.guid
+    def get_title(self):
+        return self.title
+    def get_description(self):
+        return self.description
+    def get_link(self):
+        return self.link
+    def get_pubdate(self):
+        return self.pubdate
 
 
 #======================
